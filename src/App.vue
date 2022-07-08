3027dbf1f1da5dd0c947b90e524b901ba68c0ea0
PauloAlves8039 <pj38alves@gmail.com>
2022-07-08 20:56:08 -0300

    Criando elementos e aplicando estilo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    
+    <img src="./assets/cronometro.png" class="img">
+    <a class="timer">00:00,00</a>
+
+    <div class="areaBtn">
+      <button class="botao">VAI</button>
+      <button class="botao">LIMPAR</button>
+    </div>
+
   </div>
 </template>
 
@@ -12,5 +19,47 @@ export default {
 </script>
 
 <style>
+#app {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.img{
+  width: 420px;
+  height: 420px;
+  padding-top: 100px;
+}
+
+.timer{
+  color: #FFFFFF;
+  font-size: 70px;
+  margin-top: -210px;
+}
+
+.areaBtn{
+  margin-top: 155px;
+  display: flex;
+}
+
+.botao{
+  -webkit-user-select: none;
+  width: 150px;
+  background-color: #FFF;
+  font-size: 20px;
+  border: 0;
+  border-radius: 5px;
+  text-align: center;
+  margin-left: 15px;
+  padding: 6px;
+  cursor: pointer;
+}
+
+.botao:hover{
+  opacity: 0.8;
+  transition: all 0.50s;
+}
 
 </style>
